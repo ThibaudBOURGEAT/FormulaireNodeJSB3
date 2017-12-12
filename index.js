c3e022ac19305bd848493fb9e1754424e62c3dc6
ThibaudBOURGEAT <thibaud.bourgeat.trx@gmail.com>
2017-12-12 13:57:57 +0100

    Push

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var urlParser = bodyParser.urlencoded({extended: true})
 const port = 8000;
 const app = express();
 
-app.use(express.static(path.join(__dirname + 'app/dist')));
+app.use(express.static(path.join(__dirname, 'app/dist')));
 
 app.get('/', (req, res) => {
   res.statusCode = 200;
@@ -21,13 +21,11 @@ app.get('/register', (req, res) => {
   res.end(fs.readFileSync(__dirname + '/app/src/templates/register.html'));
 });
 
-app.post('/register', urlencoderParser,(req, res) => {
+app.post('/register', urlParser,(req, res) => {
   fs.appendFile(__dirname + '/app/src/event.txt', JSON.stringify(req.body) + "\r\n" , "UTF-8");
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-
   res.end(fs.readFileSync(__dirname + '/app/src/templates/index.html'));
-
 });
 
 app.listen(port, function() {
