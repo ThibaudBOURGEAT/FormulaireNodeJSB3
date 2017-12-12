a545e6b3f7e51d58c6d1aa92f83310ed71eec23c
ThibaudBOURGEAT <thibaud.bourgeat.trx@gmail.com>
2017-12-12 11:20:36 +0100

    Push

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var urlencoderParser = bodyParser.urlencoded({ extended: false});
 
 const port = 8000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/app/src/templates/index.html')
+app.get('/register', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end(fs.readFileSync(__dirname + '/app/src/templates/index.html'));
+});
+
+app.post('/register', urlencoderParser,(req, res) => {
+  console.log("ok");
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/json; charset=utf-8');
+  res.end(JSON.stringify(req.body));
 });
 
 app.listen(port, function() {
