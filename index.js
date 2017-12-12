d84e08cd3c1fdee99b6557c2e44cc2f15d08b82b
Pierre Plessy <pierre.plessy@outlook.fr>
2017-12-12 12:28:48 +0100

    Add file

--- index.js
+++ index.js
@@ -21,9 +21,20 @@ app.get('/register', (req, res) => {
   res.end(fs.readFileSync(__dirname + '/app/src/templates/register.html'));
 });
 
-app.post('/register', urlParser, (req, res) => {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(req.body));
+app.post('/register', urlencoderParser,(req, res) => {
+  console.log("ok");
+
+  fs.appendFile(__dirname + '/app/src/event.txt', JSON.stringify(req.body) + "\r\n" , "UTF-8");
+
+  contenu = fs.readFileSync(__dirname + '/app/src/event.txt', "UTF-8");
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  console.log(contenu);
+
+  // res.end(JSON.stringify(contenu));
+
+  res.end(fs.readFileSync(__dirname + '/app/src/templates/index.html'));
+
 });
 
 app.listen(port, function() {
