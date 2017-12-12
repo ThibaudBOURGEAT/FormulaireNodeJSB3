e7220dcb5408f6700206a307e577feb452d8534f
Pierre Plessy <pierre.plessy@outlook.fr>
2017-12-12 16:13:23 +0100

    test csv

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var path = require('path');
-var urlParser = bodyParser.urlencoded({extended: true})
+var urlParser = bodyParser.urlencoded({extended: true});
+
+var csvWriter = require('csv-write-stream')
+var writer = csvWriter({headers: ["lastname" ," firstname" ,"address" , "pseudo" , "email"]});
 
 const port = 8000;
 const app = express();
@@ -10,6 +13,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'app/dist')));
 
 app.get('/', (req, res) => {
+  res.contentFile = fs.readFileSync(__dirname + '/app/src/event.txt', "UTF-8");
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end(fs.readFileSync(__dirname + '/app/src/templates/index.html'));
@@ -22,10 +27,21 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', urlParser,(req, res) => {
-  fs.appendFile(__dirname + '/app/src/event.txt', JSON.stringify(req.body) + "\r\n" , "UTF-8");
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end(fs.readFileSync(__dirname + '/app/src/templates/index.html'));
+    writer.pipe(fs.appendFileSync(__dirname + '/app/src/formList.csv'));
+    writer.write(req.body);
+    writer.end();
+
+    // fs.writeFile(__dirname + '/app/src/formList.csv', JSON.stringify(req.body), 'utf8', function (err) {
+    //     if (err) {
+    //         console.log('Some error occured - file either not saved or corrupted file saved.');
+    //     } else{
+    //         console.log('It\'s saved!');
+    //     }
+    // });
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(fs.readFileSync(__dirname + '/app/src/templates/register.html'));
 });
 
 app.listen(port, function() {
