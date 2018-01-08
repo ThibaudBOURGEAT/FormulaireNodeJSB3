4d50d003fe6ca3a79c7f6e92d37e892397708c14
Pierre Plessy <pierre.plessy@outlook.fr>
2018-01-08 14:44:50 +0100

    .env & config.js

--- zeios/index.js
+++ zeios/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 
-const port = 8000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'app/dist')));
 
 app.listen(port, function() {
-  console.log('Le serveur répond sur le port: '+ port);
+  console.log('Le serveur répond sur le port: '+ process.env.PORT);
 });
