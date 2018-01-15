b705e59f00adb4ffb9fc1fed3b4ca2113dad1d5f
Pierre Plessy <pierre.plessy@outlook.fr>
2018-01-15 16:02:56 +0100

    goup

--- zeios/api/group/group.js
+++ zeios/api/group/group.js
@@ -1,41 +1,91 @@
 const router = require('express').Router();
+const User = require('../../models/User');
+const Group = require('../../models/Group');
 
-router.get('/all', function(req,res){
-    Group.find({deleted: false}).limit(10).then(function(groups){
+router.get('/all', function(req, res) {
+    Group.find({
+        deleted: false
+    }).limit(10).then(function(groups) {
         res.json(groups);
     });
 });
 
-router.post('/register', function(req,res){
+router.post('/register', function(req, res) {
     const newGroup = new Group({
         wording: req.body.wording,
         description: req.body.description
     });
 
-    newGroup.save(function(err){
-        res.json({success: true, message: 'Group created !'});
+    newGroup.save(function(err) {
+        res.json({
+            success: true,
+            message: 'Group created !'
+        });
     });
 });
 
-router.post('/update', function(req,res){
+router.post('/update', function(req, res) {
     var description = req.body.description;
 
-    Group.find({login: req.body.wording}).update({
+    Group.find({
+        login: req.body.wording
+    }).update({
         $set: {
             description: description
         }
-    }, function(err){
-        if(err){res.json({success: false, message: 'Error'});}
-        else{res.json({success: true, message: 'Group update !'});}
+    }, function(err) {
+        if (err) {
+            res.json({
+                success: false,
+                message: 'Error'
+            });
+        } else {
+            res.json({
+                success: true,
+                message: 'Group update !'
+            });
+        }
     });
 });
 
-router.delete('/delete', function(req,res){
-    Group.remove({wording: req.body.wording}, function(err){
-        if (err){
+router.post('/addUser', (req, res) => {
+    Group.find({})
+        .then((groups) => {
+            for (group in groups) {
+                User.find({
+                        group: groups[group]._id
+                    })
+                    .then((users_group) => {
+                        for (user in users_group) {
+                            console.log(user);
+                            groups[group].users.push(user);
+                        }
+                        groups[group].save()
+                            .catch((err) => {
+                                res.json(err);
+                            })
+                    })
+
+            }
+
+            res.json({success: true, message: 'All users are add to group'});
+        })
+        .catch((err) => {
+            res.json(err);
+        })
+})
+
+router.delete('/delete', function(req, res) {
+    Group.remove({
+        wording: req.body.wording
+    }, function(err) {
+        if (err) {
             res.json("Error");
-        }else{
-            res.json({success: true, message: 'Account delete !'})
+        } else {
+            res.json({
+                success: true,
+                message: 'Account delete !'
+            })
         }
     });
 });
