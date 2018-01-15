8b20bddfc6553d0f7cd15a55009d1a611ba5a600
ThibaudBOURGEAT <thibaud.bourgeat.trx@gmail.com>
2018-01-15 16:05:49 +0100

    clean

--- zeios/api/index.js
+++ zeios/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.use('/user', require('./user'));
 router.use('/auth', require('./auth'));
-router.use('/group', require('./group'));
-router.use('/address', require('./address'));
+router.use('/group', passport.authenticate('jwt', {session: false}), require('./group'));
+router.use('/address', passport.authenticate('jwt', {session: false}), require('./address'));
 
 module.exports = router ;
